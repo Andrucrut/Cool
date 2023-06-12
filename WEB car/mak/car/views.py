6b47893d3a72f9b607fe38d351f9bee70f273215
Andrucrut <andrucrut@mail.ru>
2023-06-12 16:42:26 +0300

    ffd

---
+++ WEB car/mak/car/views.py
@@ -0,0 +1,166 @@
+from django.contrib.auth import logout, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse,HttpResponseNotFound
+from django.urls import reverse_lazy
+from django.views.generic import ListView,DetailView,CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .forms import *
+from .models import *
+from .utils import *
+
+class CarsHome(DataMixin, ListView):
+    paginate_by = 3
+    model = Cars
+    template_name = 'car/index.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Главная страница")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_queryset(self):
+        return Cars.objects.filter(is_published=True)
+
+
+'''def index(request):
+    posts = Cars.objects.all()
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница',
+        'cat_selected': 0,
+    }
+
+    return render(request, 'car/index.html', context=context)'''
+
+
+def about(request):
+    return render(request, 'car/about.html', {'menu': menu, 'title': 'О сайте'})
+
+
+class AddPage(LoginRequiredMixin,DataMixin,CreateView):
+    form_class = AddPostForm
+    template_name = 'car/addpage.html'
+    success_url = reverse_lazy('home')
+    login_url = reverse_lazy('home')
+    raise_exception = True
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Добавление статьи")
+        context = dict(list(context.items()) + list(c_def.items()))
+        return context
+
+'''def addpage(request):
+    if request.method == 'POST':
+        form = AddPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = AddPostForm()
+    return render(request, 'car/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление машины'}) #передается глав меню и титле'''
+
+'''def login(request):
+    return HttpResponse("Авторизация")'''
+
+def contact(request):
+    return render(request,"car/contack.html",{'title': "Обратная связь"})
+
+def pageNotFound(request,exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
+
+
+class ShowPost(DataMixin,DetailView):
+    model = Cars
+    template_name = 'car/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title=context['post'])
+        return dict(list(context.items()) + list(c_def.items()))
+
+'''def show_post(request, post_id):
+    post = get_object_or_404(Cars, pk=post_id) #pk = первичный ключ   get= находит пост с ключом, если нет то 404
+
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
+
+    return render(request, 'car/post.html', context=context)'''
+
+
+class CarsCategory(DataMixin,ListView):
+    model = Cars
+    template_name = 'car/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Cars.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Категория - ' + str(context['posts'][0].cat),
+                                      cat_selected=context['posts'][0].cat_id)
+
+        return dict(list(context.items()) + list(c_def.items()))
+
+'''def show_category(request, cat_id):
+    posts = Cars.objects.filter(cat_id=cat_id)
+
+
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected': cat_id,
+    }
+
+    return render(request, 'car/index.html', context=context)'''
+
+
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'car/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'car/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
